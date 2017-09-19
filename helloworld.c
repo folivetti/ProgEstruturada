fb843c65a7932e0a235e76dfdf3eabe172da42d7
Fabricio Olivetti de Franca <fabricio.olivetti@gmail.com>
2017-09-19 08:36:24 -0300

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/* helloworld.c
+*
+* Programa exemplo que imprime uma frase 
+* na saída padro.
+*/
+
+#include <stdio.h>
+
+int main( ) {
+
+    printf("Hello World\n");
+    
+    return 0;  /* a função principal sempre retorna 0 em caso de sucesso! */
+}
